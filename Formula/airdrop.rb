10c9ee08df5d136c952613f8eb089e7c326b8779
Mihai Crisan <crisanmihai2004@gmail.com>
2026-05-15 13:33:03 +0300

    airdrop: generate iconset during install when AppIcon.png is present

--- Formula/airdrop.rb
+++ Formula/airdrop.rb
@@ -17,16 +17,35 @@ class Airdrop < Formula
     system "swiftc", "-O", "-framework", "AppKit",
                      "-o", "airdrop", "Sources/main.swift"
 
-    app_dir = libexec/"Airdrop.app/Contents/MacOS"
-    app_dir.mkpath
-    app_dir.install "airdrop"
-    (libexec/"Airdrop.app/Contents").install "Info.plist"
+    contents = libexec/"Airdrop.app/Contents"
+    macos_dir = contents/"MacOS"
+    resources = contents/"Resources"
+    macos_dir.mkpath
+    resources.mkpath
+    macos_dir.install "airdrop"
+    contents.install "Info.plist"
+
+    if File.exist?("AppIcon.png")
+      iconset = buildpath/"AppIcon.iconset"
+      iconset.mkpath
+      sizes = [
+        [16,   "icon_16x16.png"],      [32,   "icon_16x16@2x.png"],
+        [32,   "icon_32x32.png"],      [64,   "icon_32x32@2x.png"],
+        [128,  "icon_128x128.png"],    [256,  "icon_128x128@2x.png"],
+        [256,  "icon_256x256.png"],    [512,  "icon_256x256@2x.png"],
+        [512,  "icon_512x512.png"],    [1024, "icon_512x512@2x.png"],
+      ]
+      sizes.each do |dim, name|
+        system "sips", "-z", dim.to_s, dim.to_s, "AppIcon.png", "--out", iconset/name, out: File::NULL
+      end
+      system "iconutil", "-c", "icns", iconset, "-o", resources/"AppIcon.icns"
+    end
 
     system "codesign", "--force", "--sign", "-", libexec/"Airdrop.app"
 
     (bin/"ad").write <<~SHELL
       #!/bin/sh
-      exec "#{app_dir}/airdrop" "$@"
+      exec "#{macos_dir}/airdrop" "$@"
     SHELL
     chmod 0755, bin/"ad"
   end
